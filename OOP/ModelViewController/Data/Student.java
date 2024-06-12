10735f0c1213a69a4907bd4de65eee1de1caa3fa
Vladimir Kiselev <kiselev@gnivc.ru>
2024-06-12 16:41:55 +0300

    Create a package – model. Implement the abstract User class and its heirs Student and Teacher.

---
+++ OOP/ModelViewController/Data/Student.java
@@ -0,0 +1,22 @@
+public class Student extends User {
+    private String studentId;
+
+    public Student(String fullName, int birthYear, String studentId) {
+        super(fullName, birthYear);
+        this.studentId = studentId;
+    }
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    @Override
+    public String toString() {
+        return "Student [studentId=" + studentId + super.toString() + "]";
+    }
+
+}
